2c68f3ee5c734b8353d3a5db301aa41ae6642b68
Michael Große <mic.grosse@googlemail.com>
2020-02-19 10:28:44 +0100

    Remove unnecessary guards and requires
    Since these files only define classes, the guards are not necessary.
    Also, DokuWiki has a robust autoloading in place since long ago, so
    custom require statements are no longer needed.

--- action.php
+++ action.php
@@ -8,10 +8,6 @@
  * @author  lisps
  */
 
-if (!defined('DOKU_INC')) die();
-if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
-require_once (DOKU_PLUGIN . 'action.php');
-
 class action_plugin_tagfilter extends DokuWiki_Action_Plugin {
 	/**
 	 * Register the eventhandlers
--- helper.php
+++ helper.php
@@ -6,14 +6,6 @@
  * @author  lisps
  */
 
-// must be run within Dokuwiki
-if (!defined('DOKU_INC')) die();
-
-if (!defined('DOKU_LF')) define('DOKU_LF', "\n");
-if (!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
-
-require_once(DOKU_INC.'inc/indexer.php');
-
 class helper_plugin_tagfilter extends DokuWiki_Plugin {
 	/**
 	 * 
