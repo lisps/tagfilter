446ee8c8459230baf51607ecac18b1d92511e2e1
Christoph H. Larsen <christoph.larsen@synalinq.com>
2020-04-14 19:01:33 +0700

    Update lang.php

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -5,7 +5,7 @@
  * @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author  lisps    
  */
-$lang['choose'] = 'choosing';
-$lang['Delete filter']='Delete filter';
+$lang['choose'] = 'selection';
+$lang['Delete filter']='Reset filter';
 $lang['missing_plugin'] = "<i>Plugin %s is not available or invalid.</i>";
 $lang['found_count'] = "Found";
