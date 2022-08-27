4dcabc7659a0af0ce2fa81265d09a9f930612929
Gerrit Uitslag <klapinklapin@gmail.com>
2022-08-28 00:17:05 +0200

    remote: update deprecated, reformatting

--- remote.php
+++ remote.php
@@ -1,4 +1,7 @@
 <?php
+
+use dokuwiki\Remote\AccessDeniedException;
+
 class remote_plugin_tagfilter extends DokuWiki_Remote_Plugin {
     public function _getMethods() {
         return array(
@@ -8,73 +11,84 @@ class remote_plugin_tagfilter extends DokuWiki_Remote_Plugin {
             )*/
         );
     }
-    
+
+    /**
+     * @throws AccessDeniedException
+     */
     public function getTagsByPage($id) {
         if(auth_quickaclcheck($id) < AUTH_READ){
-            throw new RemoteAccessDeniedException('You are not allowed to read this file', 111);
+            throw new AccessDeniedException('You are not allowed to read this file', 111);
         }
-        
-        $Htagfilter = $this->loadHelper('tagfilter',false);
-        if(!$Htagfilter) {
-            /*Exeption*/
-            throw new RemoteAccessDeniedException('problem with helper plugin', 99999);
-        } 
-        return $Htagfilter->getTagsBySiteID($id);
-    }
-    
-    public function getPagesByTags($tags,$ns='') {  
 
+        /** @var helper_plugin_tagfilter $Htagfilter */
         $Htagfilter = $this->loadHelper('tagfilter',false);
         if(!$Htagfilter) {
             /*Exeption*/
-            throw new RemoteAccessDeniedException('problem with helper plugin', 99999);
-        } 
+            throw new AccessDeniedException('problem with helper plugin', 99999);
+        }
+        return $Htagfilter->getTagsByPageID($id);
+    }
+
+    /**
+     * @throws AccessDeniedException
+     */
+    public function getPagesByTags($tags, $ns='') {
+
+        /** @var helper_plugin_tagfilter $Htagfilter */
+        $Htagfilter = $this->loadHelper('tagfilter',false);
+        if(!$Htagfilter) {
+            /*Exeption*/
+            throw new AccessDeniedException('problem with helper plugin', 99999);
+        }
 
         $pages =  $Htagfilter->getPagesByTags($ns,$tags);
 
         //$pages_cleaned = array_intersect_key($pages, array_flip('id','title'));
-        $pages_r = array();
-        
+        $pages_r = [];
+
         foreach($pages as $page){
             $title = p_get_metadata($page, 'title', METADATA_DONT_RENDER);
-            $pages_r[] = array(
-                'title' => $title?$title:$page,
+            $pages_r[] = [
+                'title' => $title?:$page,
                 'id' => $page,
-                'tags' => $Htagfilter->getTagsBySiteID($page)
-                );
+                'tags' => $Htagfilter->getTagsByPageID($page)
+            ];
         }
-        
+
         return $pages_r;
     }
-	
-	public function getPagesByRegExpTags($tags,$ns='') {  
 
+    /**
+     * @throws AccessDeniedException
+     */
+    public function getPagesByRegExpTags($tags, $ns='') {
+        /** @var helper_plugin_tagfilter $Htagfilter */
         $Htagfilter = $this->loadHelper('tagfilter',false);
         if(!$Htagfilter) {
             /*Exeption*/
-            throw new RemoteAccessDeniedException('problem with helper plugin', 99999);
-        } 
+            throw new AccessDeniedException('problem with helper plugin', 99999);
+        }
 
 
-		$tags_r = $Htagfilter->getTagsByRegExp($tags,$ns);
-		$tags_r = array_keys($tags_r);
+		$tags_labels = $Htagfilter->getTagsByRegExp($tags,$ns);
+		$tags_r = array_keys($tags_labels);
         $pages =  $Htagfilter->getPagesByTags($ns,implode(' ',$tags_r));
 
         //$pages_cleaned = array_intersect_key($pages, array_flip('id','title'));
-        $pages_r = array();
-        
+        $pages_r = [];
+
         foreach($pages as $page){
             $title = p_get_metadata($page, 'title', METADATA_DONT_RENDER);
-            $pages_r[] = array(
-                'title' => $title?$title:$page,
+            $pages_r[] = [
+                'title' => $title?:$page,
                 'id' => $page,
-                'tags' => $Htagfilter->getTagsBySiteID($page)
-                );
+                'tags' => $Htagfilter->getTagsByPageID($page)
+            ];
         }
-        
+
         return $pages_r;
     }
-    
-    
-    
+
+
+
 }
